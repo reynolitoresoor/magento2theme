1352cfd0691543541fe82ff4737801ed01e0bbee
reynolitoresoor <reynolitoresoor123@gmail.com>
2021-10-31 13:01:01 +0000

    Update home page content module and work on the home page content

---
+++ design/frontend/Acxelsus/jaybro/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {            
+            'owlcarousel': "Magento_Catalog/js/owlcarousel"
+        },   
+    shim: {
+        'owlcarousel': {
+            deps: ['jquery']
+        }
+    }
+};

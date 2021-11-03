5c0652efc4d6bdc36f0cb36429b08afb63bd28e8
reynolitoresoor <reynolitoresoor123@gmail.com>
2021-11-03 06:23:49 +0000

    Work on the cart and checkout page

--- code/Acxelsus/Jaybro/Block/HomepageContent.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Acxelsus\Jaybro\Block;
-
-class HomepageContent extends \Magento\Framework\View\Element\Template {
-	
-}
--- code/Acxelsus/Jaybro/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Acxelsus_Jaybro',
-    __DIR__
-);

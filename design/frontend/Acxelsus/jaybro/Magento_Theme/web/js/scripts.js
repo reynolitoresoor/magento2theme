96d7c5b7baaee52f5175d13a20076dc6191d1fc1
reynolitoresoor <reynolitoresoor123@gmail.com>
2021-11-23 07:49:36 +0000

    Our range submenu and about us submenu

--- design/frontend/Acxelsus/jaybro/Magento_Theme/web/js/scripts.js
+++ design/frontend/Acxelsus/jaybro/Magento_Theme/web/js/scripts.js
@@ -2,6 +2,20 @@ require([
     "jquery"
 ], function ($) {
     $(document).ready(function () {
-        alert('ready');
+        $('.navigation ul li.about-jaybro').hover(function () {
+            $('.about-us-container').fadeIn().show();
+        });
+        $('.navigation ul li.our-range').hover(function () {
+            $('.about-us-container').hide();
+        });
+        $('.navigation ul li.careers').hover(function () {
+            $('.about-us-container').hide();
+        });
+        $('.navigation ul li.contact-us').hover(function () {
+            $('.about-us-container').hide();
+        });
+        $('.navigation ul li.my-quoate').hover(function () {
+            $('.about-us-container').hide();
+        });
     });
 });

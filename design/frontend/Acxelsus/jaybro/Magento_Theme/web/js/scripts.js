c2eaac6acacf63cd265e3c41ce7610d81df2bf6c
reynolitoresoor <reynolitoresoor123@gmail.com>
2021-12-09 12:15:24 +0000

    Work on mobile responsive on every pages

--- design/frontend/Acxelsus/jaybro/Magento_Theme/web/js/scripts.js
+++ design/frontend/Acxelsus/jaybro/Magento_Theme/web/js/scripts.js
@@ -34,5 +34,13 @@ require([
             $(this).find('span').toggleClass('fa-angle-down');
             $('ul.contact-info').slideToggle();
         });
+        $('div.navicon').click(function () {
+            console.log('show sidebar');
+            $('.sidebar-main').css('left', '0');
+        });
+        $('.cmsMobNacIcoClose').click(function () {
+            console.log('hide sidebar');
+            $('.sidebar-main').css('left', '-100%');
+        });
     });
 });

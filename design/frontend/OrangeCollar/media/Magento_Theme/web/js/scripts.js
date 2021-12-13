87fac78ada0a979df518269d3073e4107fe53aa2
reynolitoresoor <reynolitoresoor123@gmail.com>
2021-12-13 01:03:20 +0000

    Update mobile menu script

--- design/frontend/OrangeCollar/media/Magento_Theme/web/js/scripts.js
+++ design/frontend/OrangeCollar/media/Magento_Theme/web/js/scripts.js
@@ -2,6 +2,13 @@ require([
     "jquery"
 ], function ($) {
     $(document).ready(function () {
-
+        $('div.navicon').click(function () {
+            console.log('show sidebar');
+            $('.sidebar-main').css('left', '0');
+        });
+        $('.cmsMobNacIcoClose').click(function () {
+            console.log('hide sidebar');
+            $('.sidebar-main').css('left', '-100%');
+        });
     });
 });
